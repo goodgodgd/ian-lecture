4f12ae7b2b14469188edce281dd59cad1a736c24
goodgodgd <goodgodgd@yonsei.ac.kr>
2019-03-09 18:01:29 +0900

    rearrange files

--- scripts/ch02_01_number.py
+++ scripts/ch02_01_number.py
@@ -16,3 +16,18 @@ print(b ** 2)
 print(a % b)
 # 나누기 후 몫
 print(a // b)
+
+
+# 연습문제: 16진수 변환
+num = 13**3
+
+# 첫째 자리
+h1 = num // (16**2)
+# 둘째 자리
+residue = num % (16**2)
+h2 = residue // 16
+# 셋째 자리
+h3 = num % 16
+
+print(h1, h2, h3)
+print(hex(num))
